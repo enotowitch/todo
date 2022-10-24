a262eb1ac3f0b41cad5cedc3000a31bb34be705f
enotowitch <en.enotowitch@gmail.com>
2022-10-24 13:37:00 +0300

    search FIX

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,62 +2,74 @@ import React from "react"
 import search from "./../img/search.svg"
 import del from "./../img/del.svg"
 import Todo from "./Todo"
+import SearchTag from "./SearchTag"
 
 export default function Search(props) {
 
-	const tasksObj = JSON.parse(document.cookie.match(/tasks={.*?}/)[0].replace('tasks=', ''))
-	const options = Object.values(tasksObj).reverse().map(elem => <option>{elem}</option>)
+	const taskObj = JSON.parse(document.cookie.match(/tasks={.*?}/)[0].replace('tasks=', ''))
+	const taskOptions = Object.values(taskObj).reverse().map(elem => <option>{elem}</option>)
 
 	const [searchState, setSearchState] = React.useState({})
 	function changeSearchState(event) {
 		const { name, value } = event.target
 		setSearchState(prevState => ({ ...prevState, [name]: value }))
-		if (name === "text") {
-			setSearchState(prevState => ({ ...prevState, select: "choose task" }))
-		}
-		if (name === "select") {
-			setSearchState(prevState => ({ ...prevState, text: "" }))
-		}
 	}
 
-	let searchedTodos
-	searchedTodos = props.todos.map((elem, ind) => {
-		let taskName
-		// check if first word === searchState.select
-		elem.text.match(/.*?\s/) && (taskName = elem.text.match(/.*?\s/)[0].trim().toLowerCase())
-		return taskName === searchState.select && <Todo showDate={true} key={ind} {...elem} action={props.action} moveTodo={props.moveTodo} setPopUpState={props.setPopUpState} setShowPopUp={props.setShowPopUp} />
+	// ! define textIds
+	let textIds = props.todos.map((todo) => {
+		return todo.text.includes(searchState.text) && todo.id
 	})
+	textIds = textIds.filter(isTrue => isTrue)
+	// ! define statusIds
+	let statusIds = props.todos.map((todo) => {
+		if (searchState.status === "all") {
+			return todo.id
+		}
+		if (searchState.status === "not done") {
+			return !todo.isDone && todo.id
+		}
+		const s = searchState.status
+		let status
+		s && (status = "is" + s.charAt(0).toUpperCase() + s.slice(1))
+		return todo[status] && todo.id
+	})
+	statusIds = statusIds.filter(isTrue => isTrue)
+	// ! define taskIds	
+	let taskIds = props.todos.map((todo) => {
+		return todo.text.match(/\w+/) == searchState.task && todo.id
+	})
+	taskIds = taskIds.filter(isTrue => isTrue)
 
-	if (searchState.select === "all") {
-		searchedTodos = props.todos.map((elem, ind) => {
-			return !elem.isHidden && <Todo showDate={true} key={ind} {...elem} action={props.action} moveTodo={props.moveTodo} setPopUpState={props.setPopUpState} setShowPopUp={props.setShowPopUp} />
-		})
+	// ! intersect
+	const intersect = []
+	textIds.length > 0 && intersect.push(textIds)
+	statusIds.length > 0 && intersect.push(statusIds)
+	taskIds.length > 0 && intersect.push(taskIds)
+
+
+	let result = []
+	for (let i = 0; i < intersect.length - 1; i++) {
+		result = intersect[i].filter(val => intersect[i + 1].includes(val))
 	}
-	// isDone
-	if (searchState.select === "done") {
-		searchedTodos = props.todos.map((elem, ind) => {
-			return elem.isDone && <Todo showDate={true} key={ind} {...elem} action={props.action} moveTodo={props.moveTodo} setPopUpState={props.setPopUpState} setShowPopUp={props.setShowPopUp} />
-		})
-	}
-	// isLiked
-	if (searchState.select === "liked") {
-		searchedTodos = props.todos.map((elem, ind) => {
-			return elem.isLiked && <Todo showDate={true} key={ind} {...elem} action={props.action} moveTodo={props.moveTodo} setPopUpState={props.setPopUpState} setShowPopUp={props.setShowPopUp} />
-		})
-	}
-	// isHidden
-	if (searchState.select === "hidden") {
-		searchedTodos = props.todos.map((elem, ind) => {
-			return elem.isHidden && <Todo showDate={true} key={ind} {...elem} action={props.action} moveTodo={props.moveTodo} setPopUpState={props.setPopUpState} setShowPopUp={props.setShowPopUp} />
-		})
-	}
-	// text
-	if (searchState.text) {
-		searchedTodos = props.todos.map((elem, ind) => {
-			return elem.text.includes(searchState.text) && <Todo showDate={true} key={ind} {...elem} action={props.action} moveTodo={props.moveTodo} setPopUpState={props.setPopUpState} setShowPopUp={props.setShowPopUp} />
+
+	// if only one search
+	intersect.length === 1 && (result = intersect[0])
+
+	// if in input/select search is some val, but no ids 
+	searchState.text && textIds.length == 0 && (result = [])
+	searchState.status && statusIds.length == 0 && (result = [])
+	searchState.task && taskIds.length == 0 && (result = [])
+
+	let searched = []
+	for (let i = 0; i < result.length; i++) {
+		props.todos.map((todo, ind) => {
+			todo.id === result[i] && searched.push(<Todo showDate={true} key={ind} {...todo} action={props.action} moveTodo={props.moveTodo} setPopUpState={props.setPopUpState} setShowPopUp={props.setShowPopUp} />)
 		})
 	}
 
+	function delTag(name) {
+		setSearchState(prevState => ({ ...prevState, [name]: "" }))
+	}
 
 
 
@@ -75,25 +87,37 @@ export default function Search(props) {
 							name="text"
 							value={searchState.text}
 							onChange={changeSearchState}
-							placeholder="todo text"
+							placeholder="text"
 						/>
 						<select
-							name="select"
-							value={searchState.select}
+							name="status"
+							value={searchState.status}
 							onChange={changeSearchState}
 						>
-							<option selected disabled>choose task</option>
-							<optgroup label="Default">
-								<option>all</option>
-								<option>done</option>
-								<option>liked</option>
-								<option>hidden</option>
-							</optgroup>
-							<optgroup label="Custom">
-								{options}
-							</optgroup>
+							<option value="" selected>status</option>
+							<option>all</option>
+							<option>done</option>
+							<option>liked</option>
+							<option>hidden</option>
+							<option>not done</option>
 						</select>
-						{searchedTodos}
+						<select
+							name="task"
+							value={searchState.task}
+							onChange={changeSearchState}
+						>
+							<option value="" selected>task</option>
+							{taskOptions}
+						</select>
+
+						<div className="search-tags">
+							{result.length > 0 && <SearchTag text={result.length} title="found" showDel={false} />}
+							{searchState.text && <SearchTag text={searchState.text} title="text" showDel={true} delTag={delTag} />}
+							{searchState.status && <SearchTag text={searchState.status} title="status" showDel={true} delTag={delTag} />}
+							{searchState.task && <SearchTag text={searchState.task} title="task" showDel={true} delTag={delTag} />}
+						</div>
+
+						{searched}
 					</>
 
 				</>
---
+++ src/components/SearchTag.js
@@ -0,0 +1,12 @@
+import React from "react"
+import del from "./../img/del.svg"
+
+export default function SearchTag(props) {
+	return (
+		<div className="search-tag">
+			<span className="search-tag__title">{props.title}:</span>
+			<span className="search-tag__text">{props.text}</span>
+			{props.showDel && <img className="search-tag_del" src={del} onClick={() => props.delTag(props.title)} />}
+		</div>
+	)
+}
