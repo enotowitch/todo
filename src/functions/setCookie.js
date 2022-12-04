5fdb62683a779f7b2f844bfb8332b3e0116ff8da
enotowitch <en.enotowitch@gmail.com>
2022-12-04 12:27:07 +0200

    +cookie FIX

--- src/functions/setCookie.js
+++ src/functions/setCookie.js
@@ -1,5 +1,5 @@
 export default function setCookie(string) {
 	var expires = new Date(Date.now() + 473040000 * 1000); // + 15 years, 2022=>2037
 
-	document.cookie = `${string} expires=${expires}`
+	document.cookie = `${string}; expires=${expires}; max-age=473040000; path=/; SameSite=None; Secure;`
 }
